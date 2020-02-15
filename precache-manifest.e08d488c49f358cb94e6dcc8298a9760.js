7cb2f3864c06943c9ca3f02179cf45c314977044
Islam Mostafa <islamghany3@gmail.com>
2020-02-15 23:45:09 +0200

    Updates

---
+++ precache-manifest.e08d488c49f358cb94e6dcc8298a9760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f07709793cef1307e6dbbb6ca57af5",
+    "url": "/componentTest/index.html"
+  },
+  {
+    "revision": "682f5571186f97a5a6b1",
+    "url": "/componentTest/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "96182778f1bb8066cfaa",
+    "url": "/componentTest/static/css/main.25081558.chunk.css"
+  },
+  {
+    "revision": "682f5571186f97a5a6b1",
+    "url": "/componentTest/static/js/2.853de166.chunk.js"
+  },
+  {
+    "revision": "96182778f1bb8066cfaa",
+    "url": "/componentTest/static/js/main.910436fd.chunk.js"
+  },
+  {
+    "revision": "05154931ad70ca4cf946",
+    "url": "/componentTest/static/js/runtime~main.23a270b0.js"
+  },
+  {
+    "revision": "45276424660c5136ddddb5fcabc8aa66",
+    "url": "/componentTest/static/media/spinner.45276424.gif"
+  }
+]);
